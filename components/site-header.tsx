27a230c195cf87a8694099b76932fd49fe27fe27
envyvox <kkxo.mail@gmail.com>
2023-06-25 16:29:26 +0300

    added links

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link"
+import { Github, Twitter } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
 
 import { ThemeToggle } from "./theme-toggle"
+import { buttonVariants } from "./ui/button"
 
 export function SiteHeader() {
   return (
@@ -15,6 +17,18 @@ export function SiteHeader() {
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
+            <Link
+              className={buttonVariants({ variant: "ghost" })}
+              href={siteConfig.links.twitter}
+            >
+              <Twitter />
+            </Link>
+            <Link
+              className={buttonVariants({ variant: "ghost" })}
+              href={siteConfig.links.github}
+            >
+              <Github />
+            </Link>
             <ThemeToggle />
           </nav>
         </div>
--- config/site.ts
+++ config/site.ts
@@ -1,10 +1,11 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js Tasks App",
-  description: "",
+  name: "Next Drag & Drop",
+  description:
+    "Simple drag&drop example using nextjs, tailwind, shadcn-ui, appwrite, react-beaduful-dnd and vercel.",
   links: {
     twitter: "https://twitter.com/envyvox",
-    github: "https://github.com/envyvox",
+    github: "https://github.com/envyvox/next-dnd",
   },
 }
